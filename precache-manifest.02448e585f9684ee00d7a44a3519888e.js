7a60187e1f79563b79d58207dedadc94ac56a1c4
josprins <jco.prins@gmail.com>
2020-04-17 16:42:25 +0200

    Updates

---
+++ precache-manifest.02448e585f9684ee00d7a44a3519888e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd6e9aae79db43ed735e65e55dddc82",
+    "url": "/quiz_app/index.html"
+  },
+  {
+    "revision": "2f53cc4ccaa7e6c4e184",
+    "url": "/quiz_app/static/css/main.9dbda780.chunk.css"
+  },
+  {
+    "revision": "dc1854763887a008e385",
+    "url": "/quiz_app/static/js/2.44bb1dae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz_app/static/js/2.44bb1dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f53cc4ccaa7e6c4e184",
+    "url": "/quiz_app/static/js/main.029560cd.chunk.js"
+  },
+  {
+    "revision": "9b9924b8ea0376dde018",
+    "url": "/quiz_app/static/js/runtime-main.9e9ce5a1.js"
+  }
+]);
